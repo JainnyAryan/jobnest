ae316f01363071a7cbafb5309af527c4c7b20492
samarthverma1813@gmail.com <samarthverma1813@gmail.com>
2023-11-04 23:09:52 +0530

    Updated login and signup page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
 import loginAnim from "../assets/anims/loginAnim.json";
 import Lottie from "react-lottie";
+import Navbar from "../components/landing_page/Navbar";
 import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
   const navigate = useNavigate();
   const [isEmployer, setIsEmployer] = useState(false);
-  const onCheck = (event) => {
-    setIsEmployer(event.target.checked);
-    console.log(event.target.checked);
-  }
+  // const onCheck = (event) => {
+  //   setIsEmployer(event.target.checked);
+  //   console.log(event.target.checked);
+  // }
 
+  // Change the code here. Instead, verify the Employee or Employer using theie username from the database.
   const onClick = () => {
     if(isEmployer){
       navigate('/employer');
@@ -21,16 +23,18 @@ const LoginPage = () => {
   }
 
   return (
+    <div>
+<Navbar />
     <div
-      style={{ border: "1px solid grey", borderRadius: "20px", width: "70%" }}
-    >
+      style={{ borderRadius: "20px", width: "100%" }}
+      >
       <div
         style={{
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
         }}
-      >
+        >
         <div>
           <Lottie
             options={{
@@ -52,51 +56,53 @@ const LoginPage = () => {
           />
         </div>
 
-        <div className="card" style={{ width: "30%", border: "none" }}>
+        <div className="card" style={{ width: "25%", borderRadius: "20px" , background: "rgb(6, 58, 66)" , color: "white" }}>
           <div
             className="card-body"
             style={{ fontFamily: "Nunito Sans, sans-serif" }}
-          >
-            <h1 className="card-title text-center mb-2">
+            >
+            <img src="https://i.ibb.co/C1TNMMY/LOGO-transparent.png" alt="LOGO-transparent" border="0" width={"100%"}></img>
+            {/* <h1 className="card-title text-center mb-2">
               <strong>Login To JobNest</strong>
-            </h1>
-            <h6 className="text-center" style={{ color: "gray" }}>
-              Now you can apply for your dream job here in JobNest
-            </h6>
+            </h1> */}
+            <h4 className="text-center" style={{ color: "white " }}>
+              <strong>Welcome back to JobNest</strong>
+            </h4>
             <div style={{ display: "flex", flexDirection: "column" }}>
               <div className="form-group">
-                <label htmlFor="username">UserName</label>
+                <label htmlFor="username"><strong>Username</strong></label>
                 <input
                   type="text"
                   id="username"
-                  placeholder="Enter UserName"
+                  placeholder="Enter the Username"
                   className="form-control"
-                  style={{ borderRadius: "10px" }}
-
-                />
+                  style={{ borderRadius: "8px", border: "1px solid grey", background: "#caf0f8"}}
+                  
+                  />
               </div>
 
-              <div className="form-group mt-3">
+              {/* <div className="form-group mt-3">
                 <label htmlFor="email">Email</label>
                 <input
-                  type="text"
-                  id="email"
-                  placeholder="Enter Email"
-                  className="form-control"
-                  style={{ borderRadius: "10px" }}
+                type="text"
+                id="email"
+                placeholder="Enter Email"
+                className="form-control"
+                style={{ borderRadius: "10px" }}
                 />
-              </div>
+              </div> */}
+
               <div className="form-group mt-3">
-                <label htmlFor="password">Password</label>
+                <label htmlFor="password"><strong>Password</strong></label>
                 <input
                   type="password"
                   id="password"
-                  placeholder="Enter Password"
+                  placeholder="Enter the Password"
                   className="form-control"
-                  style={{ borderRadius: "10px" }}
+                  style={{ borderRadius: "8px", border: "1px solid grey", background: "#caf0f8", textAlign: "left" }}
                 />
               </div>
-              <span><input type="checkbox" onChange={onCheck} checked={isEmployer} />I am an Employer</span>
+              {/* <span><input type="checkbox" onChange={onCheck} checked={isEmployer} /> Signup as an Employer</span> */}
               <button
                 className="btn btn-primary mt-4"
                 style={{ borderRadius: "10px" }}
@@ -109,6 +115,7 @@ const LoginPage = () => {
         </div>
       </div>
     </div>
+                </div>
   );
 }
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Routes, Route, Switch, BrowserRouter } from "react-router-dom";
-
 import LandingPage from "../pages/LandingPage";
 import LoginPage from "../pages/LoginPage";
 import EmployeeScreen from "../pages/EmployeeScreen";
 import EmployerScreen from "../pages/EmployerScreen";
+import SignupPage from "../pages/SignupPage";
 
 function AppRoutes() {
   return (
@@ -12,6 +12,7 @@ function AppRoutes() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<SignupPage />}/>
         <Route path="/employer" element={<EmployerScreen />} />
         <Route path="/employee" element={<EmployeeScreen/>} />
       </Routes>
