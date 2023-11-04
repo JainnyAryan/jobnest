9655b059010b4a34130f36a35fa18da165f09fbc
Shravani Patil <shravani19092003@gmail.com>
2023-11-04 23:45:03 +0530

    abc1

--- src/components/employer/NoOfJobs.jsx
+++ src/components/employer/NoOfJobs.jsx
@@ -6,20 +6,21 @@ export default function NoOfJobs() {
     <div className='fullCard' style={{display:"flex", width:"100%", justifyContent:'space-evenly'}}>
       <div class="jobCard1">
 
-        <h3>40</h3>
+        <h3>254</h3>
         <p>Job Posted</p>
       </div>
       <div class="jobCard2">
-      <h3>40</h3>
-        <p>Job Posted</p>
+      <h3>2545</h3>
+        <p>Applicants</p>
       </div>
       <div class="jobCard1">
-      <h3>40</h3>
-        <p>Job Posted</p>
+      <h3>220</h3>
+        <p>Jobs Closed</p>
       </div>
       <div class="jobCard2">
-      <h3>40</h3>
-        <p>Job Posted</p>
+        <i></i>
+      <h3>75%</h3>
+        <p>Hired Success</p>
       </div>
     </div>
   )
