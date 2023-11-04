a7e2105466d7e20abf394651fd98243535f3d076
Shravani Patil <shravani19092003@gmail.com>
2023-11-05 00:57:41 +0530

    emplr jobs posted changes

--- src/components/employer/JobsPosted.jsx
+++ src/components/employer/JobsPosted.jsx
@@ -1,78 +1,137 @@
-import React from 'react'
-import "./styles/JobsPosted.css"
+import React from "react";
+import "./styles/JobsPosted.css";
+import { FaStopwatch, FaMoneyBillAlt, FaMapMarkerAlt, FaLaptop } from "react-icons/fa";
+import { IconContext } from "react-icons";
 
 export default function JobsPosted() {
-
-    const jobs = [
-        {
-            role:"Web Developer",
-            time:"Full Time",
-            location:"Germany",
-            type:"Remote",
-            salary:"20000",
-        },
-        {
-            role:"Web Developer",
-            time:"Full Time",
-            location:"Germany",
-            type:"Remote",
-            salary:"20000",
-        },
-        {
-            role:"Web Developer",
-            time:"Full Time",
-            location:"Germany",
-            type:"Remote",
-            salary:"20000",
-        },
-        {
-            role:"Web Developer",
-            time:"Full Time",
-            location:"Germany",
-            type:"Remote",
-            salary:"20000",
-        },
-        {
-            role:"Web Developer",
-            time:"Full Time",
-            location:"Germany",
-            type:"Remote",
-            salary:"20000",
-        },
-        {
-            role:"Web Developer",
-            time:"Full Time",
-            location:"Germany",
-            type:"Remote",
-            salary:"20000",
-        }
-]
+  const jobs = [
+    {
+      role: "Web Developer",
+      time: "Full Time",
+      location: "Germany",
+      type: "Remote",
+      salary: "20000",
+    },
+    {
+      role: "UI UX Developer",
+      time: "Full Time",
+      location: "Germany",
+      type: "Remote",
+      salary: "20000",
+    },
+    {
+      role: "SDE",
+      time: "Full Time",
+      location: "Germany",
+      type: "Remote",
+      salary: "20000",
+    },
+    {
+      role: "ML Intern",
+      time: "Full Time",
+      location: "Germany",
+      type: "Remote",
+      salary: "20000",
+    },
+    {
+      role: "Data Analyst",
+      time: "Full Time",
+      location: "Germany",
+      type: "Remote",
+      salary: "20000",
+    },
+    {
+      role: "Bussiness Analyst",
+      time: "Full Time",
+      location: "Germany",
+      type: "Remote",
+      salary: "20000",
+    },
+    {
+      role: "Bussiness Analyst",
+      time: "Full Time",
+      location: "Germany",
+      type: "Remote",
+      salary: "20000",
+    },
+  ];
 
   return (
     <center>
-
-    <div style={{display:"grid", flexDirection:"row",  gridTemplateColumns: 'repeat(3, 1fr)'}}>
-        {jobs.map((e)=>{
-            return(
-                <div class="jobPosted">
-                    <h3>{e.role}</h3>
-                    <center>
-
-                    <div style={{display: "flex", justifyContent:"center"}}>
-                        <div>{e.location}</div>
-                        <div>{e.salary}</div>
-                    </div>
-                    </center>
-
-                    
-                    <div style={{display: "flex", justifyContent:"center"}}>
-                        <div>{e.time}</div>
-                        <div>{e.type}</div>
-                    </div>
+      <div
+        className="allCards"
+        style={{
+          display: "grid",
+          flexDirection: "row",
+          gridTemplateColumns: "repeat(3, 1fr)",
+        }}
+      >
+        {jobs.map((e) => {
+          return (
+            <div class="jobPosted">
+              <h3>{e.role}</h3>
+              <center>
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                  <div style={{display:"flex"}}>
+                    <IconContext.Provider
+                      value={{
+                        color: "",
+                        className: "global-class-name",
+                        size: "1em",
+                      }}
+                    >
+                      <div>
+                        <FaStopwatch />
+                      </div>
+                    </IconContext.Provider>
+                    {e.location}
+                  </div>
+                  <div style={{display:"flex"}}>
+                  <IconContext.Provider
+                      value={{
+                        color: "",
+                        className: "global-class-name",
+                        size: "1em",
+                      }}
+                    >
+                      <div>
+                        <FaMoneyBillAlt />
+                      </div>
+                    </IconContext.Provider>
+                    {e.salary}</div>
                 </div>
-            )
+              </center>
+
+              <div style={{ display: "flex", justifyContent: "center" }}>
+              <div style={{display:"flex"}}>
+                  <IconContext.Provider
+                      value={{
+                        color: "",
+                        className: "global-class-name",
+                        size: "1em",
+                      }}
+                    >
+                      <div>
+                        < FaMapMarkerAlt/>
+                      </div>
+                    </IconContext.Provider>{e.time}</div>
+                    <div style={{display:"flex"}}>
+                  <IconContext.Provider
+                      value={{
+                        color: "",
+                        className: "global-class-name",
+                        size: "1em",
+                      }}
+                    >
+                      <div>
+                        <FaLaptop/>
+                      </div>
+                    </IconContext.Provider>{e.type}</div>
+              </div>
+            </div>
+          );
         })}
-    </div>
+      </div>
     </center>
-  )
+  );
 }
--- src/components/employer/NoOfJobs.jsx
+++ src/components/employer/NoOfJobs.jsx
@@ -1,27 +1,82 @@
-import React from 'react'
-import "./styles/NoOfJobs.css"
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./styles/NoOfJobs.css";
+import { FaBeer } from "react-icons/fa";
+import { IconContext } from "react-icons";
+import { IoIosCheckmarkCircle } from 'react-icons/io';
+import { FaWindowClose } from 'react-icons/fa';
+import { FaUser } from 'react-icons/fa';
+import { FaFileAlt } from 'react-icons/fa';
+
+
+
 
 export default function NoOfJobs() {
   return (
-    <div className='fullCard' style={{display:"flex", width:"100%", justifyContent:'space-evenly'}}>
-      <div class="jobCard1">
+    <div
+      className="fullCard"
+      style={{ display: "flex", width: "100%", justifyContent: "space-evenly" }}
+    >
+      <div className="jobCard1">
+      <IconContext.Provider value={{ color: "", className: "global-class-name", size:"2em" }}>
+          <div>
+            <FaFileAlt />
+          </div>
+        </IconContext.Provider>
+        <div className="mt-2">
+          <center>
 
-        <h3>254</h3>
-        <p>Job Posted</p>
+          <h3 >254</h3>
+          <p>Job Posted</p>
+          </center>
+        </div>
       </div>
-      <div class="jobCard2">
-      <h3>2545</h3>
+      <div className="jobCard2">
+      <IconContext.Provider value={{ color: "", className: "global-class-name", size:"2em" }}>
+          <div>
+            <FaUser />
+          </div>
+        </IconContext.Provider>
+        <div className="mt-2">
+          <center>
+
+        <h3>2545</h3>
         <p>Applicants</p>
+          </center>
+        </div>
+        
       </div>
-      <div class="jobCard1">
-      <h3>220</h3>
+      <div className="jobCard1">
+      <IconContext.Provider value={{ color: "", className: "global-class-name", size:"2em" }}>
+          <div>
+            <FaWindowClose />
+          </div>
+        </IconContext.Provider>
+        <div className="mt-2">
+          <center>
+
+        <h3>220</h3>
         <p>Jobs Closed</p>
+          </center>
+        </div>
+        
       </div>
-      <div class="jobCard2">
-        <i></i>
-      <h3>75%</h3>
+      <div className="jobCard2">
+        {/* <FontAwesomeIcon icon="fa-solid fa-image" />         */}
+        <IconContext.Provider value={{ color: "", className: "global-class-name", size:"2em" }}>
+          <div>
+            <IoIosCheckmarkCircle />
+          </div>
+        </IconContext.Provider>
+        <div className="mt-2">
+          <center>
+
+        <h3>75%</h3>
         <p>Hired Success</p>
+          </center>
+        </div>
+        
       </div>
     </div>
-  )
+  );
 }
