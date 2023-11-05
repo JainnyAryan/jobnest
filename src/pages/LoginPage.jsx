d1e282dea105dda152bae01c4e1fead5ce7a649e
samarthverma1813@gmail.com <samarthverma1813@gmail.com>
2023-11-05 18:35:38 +0530

    Login and Signup page frontend done

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -6,107 +6,156 @@ import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
   const navigate = useNavigate();
-  const [username, setUsername] = useState('');
-
+  const [username, setUsername] = useState("");
 
   const handleUsername = (event) => {
     setUsername(event.target.value);
     console.log(event.target.value);
-  }
+  };
 
   const onClick = () => {
     if (username === "employer") {
       navigate("/employer");
-    }
-    else if (username === "employee") {
+    } else if (username === "employee") {
       navigate("/employee");
     }
-  }
+  };
 
   return (
-    <div>
-      <div
-        style={{ borderRadius: "20px", width: "100%" }}
-      >
+    <body
+      style={{
+        backgroundImage:
+          'url("/background1.jpg")',
+        // filter: 'blur(8px)',
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        display: "flex",
+        justifyContent: "center",
+        margin: "auto",
+        height: "100%",
+      }}
+    >
+      <div>
         <div
           style={{
-            display: "flex",
+            borderRadius: "10px",
+            width: "90%",
             justifyContent: "center",
             alignItems: "center",
+            background: "#f8f9fa",
+            margin: "auto",
+            boxShadow: "3px 3px 10px",
+            marginTop: "3%",
+            marginBottom: "10%",
           }}
         >
-          <div>
-            <Lottie
-              options={{
-                loop: true,
-                autoplay: true,
-                animationData: loginAnim,
-                rendererSettings: {
-                  preserveAspectRatio: "xMidYMid slice",
-                },
-              }}
-              isClickToPauseDisabled={true}
-              style={{
-                padding: "0px",
-                margin: "0px",
-                height: "40vw",
-                width: "40vw",
-                cursor: "default"
-              }}
-            />
-          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              marginRight: "7%"
+            }}
+          >
+            <div>
+              <Lottie
+                options={{
+                  loop: true,
+                  autoplay: true,
+                  animationData: loginAnim,
+                  rendererSettings: {
+                    preserveAspectRatio: "xMidYMid slice",
+                  },
+                }}
+                isClickToPauseDisabled={true}
+                style={{
+                  padding: "0px",
+                  margin: "0px",
+                  height: "40vw",
+                  width: "40vw",
+                  cursor: "default",
+                }}
+              />
+            </div>
 
-          <div className="card" style={{ width: "25%", borderRadius: "20px", background: "rgb(6, 58, 66)", color: "white" }}>
             <div
-              className="card-body"
-              style={{ fontFamily: "Nunito Sans, sans-serif" }}
+              className="card"
+              style={{
+                width: "100%",
+                borderRadius: "5px",
+                margin: "4%",
+                borderColor: "white",
+                boxShadow: "1px 1px 30px #e9ecef",
+                // marginRight: "5%"
+              }}
             >
-              <img src="https://i.ibb.co/C1TNMMY/LOGO-transparent.png" alt="LOGO-transparent" border="0" width={"100%"}></img>
-              {/* <h1 className="card-title text-center mb-2">
+              <div
+                className="card-body"
+                style={{ fontFamily: "Signika Negative, Arial, sans-serif" }}
+              >
+                <img
+                  src="/logo_black.png"
+                  alt="LOGO-transparent"
+                  border="0"
+                  width={"100%"}
+                ></img>
+                {/* <h1 className="card-title text-center mb-2">
               <strong>Login To JobNest</strong>
             </h1> */}
-              <h4 className="text-center" style={{ color: "white " }}>
-                <strong>Welcome back to JobNest</strong>
-              </h4>
-              <div style={{ display: "flex", flexDirection: "column" }}>
-                <div className="form-group">
-                  <label htmlFor="username"><strong>Username</strong></label>
-                  <input
-                    type="text"
-                    id="username"
-                    placeholder="Enter the Username"
-                    className="form-control"
-                    style={{ borderRadius: "8px", border: "1px solid grey", background: "#caf0f8" }}
-                    onChange={handleUsername}
-                  />
-                </div>
+                <h5 className="text-center" style={{ color: "grey " }}>
+                  <strong>Login to JobNest</strong>
+                </h5>
+                <div style={{ display: "flex", flexDirection: "column" }}>
+                  <div className="form-group">
+                    <label htmlFor="username">
+                      <strong>Username</strong>
+                    </label>
+                    <input
+                      type="text"
+                      id="username"
+                      placeholder="Enter the Username"
+                      className="form-control"
+                      style={{
+                        borderRadius: "8px",
+                        border: "2px solid grey",
+                      }}
+                      onChange={handleUsername}
+                    />
+                  </div>
 
-
-                <div className="form-group mt-3">
-                  <label htmlFor="password"><strong>Password</strong></label>
-                  <input
-                    type="password"
-                    id="password"
-                    placeholder="Enter the Password"
-                    className="form-control"
-                    style={{ borderRadius: "8px", border: "1px solid grey", background: "#caf0f8", textAlign: "left" }}
-                  />
+                  <div className="form-group mt-3">
+                    <label htmlFor="password">
+                      <strong>Password</strong>
+                    </label>
+                    <input
+                      type="password"
+                      id="password"
+                      placeholder="Enter the Password"
+                      className="form-control"
+                      style={{
+                        borderRadius: "8px",
+                        border: "2px solid grey",
+                        textAlign: "left",
+                      }}
+                    />
+                  </div>
+                  {/* <span><input type="checkbox" onChange={onCheck} checked={isEmployer} /> Signup as an Employer</span> */}
+                  <button
+                    className="btn btn-primary mt-4"
+                    style={{ borderRadius: "10px" }}
+                    onClick={onClick}
+                  >
+                    Login
+                  </button>
                 </div>
-                {/* <span><input type="checkbox" onChange={onCheck} checked={isEmployer} /> Signup as an Employer</span> */}
-                <button
-                  className="btn btn-primary mt-4"
-                  style={{ borderRadius: "10px" }}
-                  onClick={onClick}
-                >
-                  Login
-                </button>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </body>
   );
-}
+};
 
 export default LoginPage;
--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -20,165 +20,202 @@ const LoginPage = () => {
   };
 
   return (
-    <body style={{backgroundImage: "linear-gradient(to right top, #0de4eb, #00cff1, #00b8f3, #00a0ee, #3285df)", backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center", display: "flex", justifyContent: "center", margin: "auto", height: "100%"}}>
+    <body
+      style={{
+        backgroundImage:
+          'url("/background1.jpg")',
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        display: "flex",
+        justifyContent: "center",
+        margin: "auto",
+        height: "100%",
+      }}
+    >
       <img src="/backgound1.jpg" alt="" />
-    <div >
+      <div>
         {/* <Navbar/> */}
-    <div
-      style={{ borderRadius: "10px", width: "90%", justifyContent: "center", alignItems: "center", background: "#f8f9fa", margin: "auto", boxShadow: "3px 3px 10px", marginTop: "3%", marginBottom: "3%"}}
-      >
         <div
           style={{
-            display: "flex",
+            borderRadius: "10px",
+            width: "90%",
             justifyContent: "center",
-            alignItems: "center"
+            alignItems: "center",
+            background: "#f8f9fa",
+            margin: "auto",
+            boxShadow: "3px 3px 10px",
+            marginTop: "3%",
+            marginBottom: "10%",
           }}
         >
-        <div>
-          <Lottie
-            options={{
-              loop: true,
-                autoplay: true,
-                animationData: loginAnim,
-                rendererSettings: {
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              marginRight: "3%"
+            }}
+          >
+            <div>
+              <Lottie
+                options={{
+                  loop: true,
+                  autoplay: true,
+                  animationData: loginAnim,
+                  rendererSettings: {
                     preserveAspectRatio: "xMidYMid slice",
                   },
-            }}
-            isClickToPauseDisabled={true}
-            style={{
-                padding: "0px",
-                margin: "0px",
-                height: "40vw",
-              width: "40vw",
-              cursor: "default",
-            }}
-            />
-          </div>
-
-        <div className="card" style={{ width: "100%", borderRadius: "5px"  , margin: "4%", borderColor: "white", boxShadow: "1px 1px 30px #e9ecef"}}>
-          <div
-            className="card-body"
-            style={{ fontFamily: "Signika Negative, Arial, sans-serif" }}
-            >
-            <img
-              src="/logo_black.png"
-              alt="LOGO-transparent"
-              border="0"
-              width={"60%"}
-              style={{ marginLeft: "20%"}}
-              ></img>
-            <div style={{ display: "flex", flexDirection: "column", textAlign: "left" }}>
-
-                {/* User enters his/her official name. */}
-                <div className="form-group mt-3">
-                  <label htmlFor="name">
-                    <strong>Name</strong>
-                  </label>
-                  <input
-                    type="text"
-                    id="name"
-                    placeholder="Enter your name"
-                    className="form-control"
-                    style={{
-                      borderRadius: "8px",
-                      border: "2px solid grey",
-                      alignItems: "start"
-                    }}
-                  />
-                </div>
-
-                {/* User enters his/her account name for jobnest platform. */}
-              <div className="form-group mt-3" style={{ color: "black"}}>
-                <label htmlFor="username">
-                  <strong>Username</strong>
-                </label>
-                <input
-                  type="text"
-                  id="username"
-                  placeholder="Enter your Username"
-                  className="form-control"
-                  style={{
-                    borderRadius: "8px",
-                    border: "2px solid grey",
                 }}
-                />
-              </div>
+                isClickToPauseDisabled={true}
+                style={{
+                  padding: "0px",
+                  margin: "0px",
+                  height: "40vw",
+                  width: "40vw",
+                  cursor: "default",
+                }}
+              />
+            </div>
 
-                {/* User enters his/her email address */}
-                <div className="form-group mt-3">
-                  <label htmlFor="email">
-                    <strong>Email</strong>
-                  </label>
-                  <input
-                    type="text"
-                    id="email"
-                    placeholder="Enter your Email"
-                    className="form-control"
-                    style={{
-                      borderRadius: "8px",
-                      border: "2px solid grey",
-                    }}
-                  />
-                </div>
-
-                {/* User enters his/her password for their account on jobnest. */}
-                <div className="form-group mt-3">
-                  <label htmlFor="password">
-                    <strong>Password</strong>
-                  </label>
-                  <input
-                    type="password"
-                    id="password"
-                    placeholder="Enter the Password"
-                    className="form-control"
-                    style={{
-                      borderRadius: "8px",
-                      border: "2px solid grey",
-                      textAlign: "left",
-                    }}
-                  />
-                </div>
-
-                {/* User enters his/her confirmed password for their account on jobnest. */}
-                <div className="form-group mt-3">
-                  <label htmlFor="password">
-                    <strong>Confirm Password</strong>
-                  </label>
-                  <input
-                    type="password"
-                    id="password"
-                    placeholder="Enter the confirm Password"
-                    className="form-control"
-                    style={{
-                      borderRadius: "8px",
-                      border: "2px solid grey",
+            <div
+              className="card"
+              style={{
+                width: "100%",
+                borderRadius: "5px",
+                margin: "4%",
+                borderColor: "white",
+                boxShadow: "1px 1px 30px #e9ecef",
+              }}
+            >
+              <div
+                className="card-body"
+                style={{ fontFamily: "Signika Negative, Arial, sans-serif" }}
+              >
+                <img
+                  src="/logo_black.png"
+                  alt="LOGO-transparent"
+                  border="0"
+                  width={"60%"}
+                  style={{ marginLeft: "20%" }}
+                ></img>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
                     textAlign: "left",
-                    }}
-                  />
-                </div>
-                <br />
-                {/* User registers either as an employer or a job seeker. */}
-                <span>
-                  <input
-                    type="checkbox"
-                    onChange={onCheck}
-                    checked={isEmployer}
-                  />{" "}
-                Signup as an Employer
-              </span>
-              <button
-                className="btn btn-primary mt-4"
-                style={{ borderRadius: "10px" }}
-                onClick={onClick}
+                  }}
                 >
-                <strong>Login</strong>
-              </button>
+                  {/* User enters his/her official name. */}
+                  <div className="form-group mt-3">
+                    <label htmlFor="name">
+                      <strong>Name</strong>
+                    </label>
+                    <input
+                      type="text"
+                      id="name"
+                      placeholder="Enter your name"
+                      className="form-control"
+                      style={{
+                        borderRadius: "8px",
+                        border: "2px solid grey",
+                        alignItems: "start",
+                      }}
+                    />
+                  </div>
+
+                  {/* User enters his/her account name for jobnest platform. */}
+                  <div className="form-group mt-3" style={{ color: "black" }}>
+                    <label htmlFor="username">
+                      <strong>Username</strong>
+                    </label>
+                    <input
+                      type="text"
+                      id="username"
+                      placeholder="Enter your Username"
+                      className="form-control"
+                      style={{
+                        borderRadius: "8px",
+                        border: "2px solid grey",
+                      }}
+                    />
+                  </div>
+
+                  {/* User enters his/her email address */}
+                  <div className="form-group mt-3">
+                    <label htmlFor="email">
+                      <strong>Email</strong>
+                    </label>
+                    <input
+                      type="text"
+                      id="email"
+                      placeholder="Enter your Email"
+                      className="form-control"
+                      style={{
+                        borderRadius: "8px",
+                        border: "2px solid grey",
+                      }}
+                    />
+                  </div>
+
+                  {/* User enters his/her password for their account on jobnest. */}
+                  <div className="form-group mt-3">
+                    <label htmlFor="password">
+                      <strong>Password</strong>
+                    </label>
+                    <input
+                      type="password"
+                      id="password"
+                      placeholder="Enter the Password"
+                      className="form-control"
+                      style={{
+                        borderRadius: "8px",
+                        border: "2px solid grey",
+                        textAlign: "left",
+                      }}
+                    />
+                  </div>
+
+                  {/* User enters his/her confirmed password for their account on jobnest. */}
+                  <div className="form-group mt-3">
+                    <label htmlFor="password">
+                      <strong>Confirm Password</strong>
+                    </label>
+                    <input
+                      type="password"
+                      id="password"
+                      placeholder="Enter the confirm Password"
+                      className="form-control"
+                      style={{
+                        borderRadius: "8px",
+                        border: "2px solid grey",
+                        textAlign: "left",
+                      }}
+                    />
+                  </div>
+                  <br />
+                  {/* User registers either as an employer or a job seeker. */}
+                  <span>
+                    <input
+                      type="checkbox"
+                      onChange={onCheck}
+                      checked={isEmployer}
+                    />{" "}
+                    Signup as an Employer
+                  </span>
+                  <button
+                    className="btn btn-primary mt-4"
+                    style={{ borderRadius: "10px" }}
+                    onClick={onClick}
+                  >
+                    <strong>Login</strong>
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    </div>
     </body>
   );
 };
