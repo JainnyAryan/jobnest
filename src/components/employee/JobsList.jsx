88cf8c4b40b27457d733d493863f0bef83e68446
Aryan Jain <39268389+JainnyAryan@users.noreply.github.com>
2023-11-11 05:05:50 +0530

    f

--- src/components/employee/JobsList.jsx
+++ src/components/employee/JobsList.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import { MdLocationPin } from "react-icons/md";
 
 import styles from "./styles/JobList.module.css";
-import { TransitionGroup } from "react-transition-group";
-import { CSSTransition } from "react-transition-group";
 import { CircularProgress } from "@mui/material";
 
 const JobsList = (props) => {
   const jobItems = props.jobItems;
+  console.log(jobItems);
+  const defaultImgUrl = "/LOGO.jpeg";
 
   return (
     <div className={styles.box}>
       {jobItems ? jobItems.map((e) => {
         return (
           <div onClick={() => props.onClick(e)} className={e != props.selectedJob ? `${styles.jobTile}` : `${styles.jobTile} ${styles.selected}`}>
-            <img src={e.iconUrl} style={{ height: "10vh", objectFit: "contain", aspectRatio: "1/1", marginRight: "20px" }} />
+            <img src={e.iconUrl == null ? e.iconUrl : defaultImgUrl} style={{ height: "10vh", objectFit: "contain", aspectRatio: "1/1", marginRight: "20px" }} />
             <div style={{ flex: 3, display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
               <div style={{ display: "flex", height: "10vh", flexDirection: "column", alignItems: "start", justifyContent: "space-evenly", width: "50%" }}>
                 <p style={{ margin: "0px", color: "grey" }}>{e.role}</p>
