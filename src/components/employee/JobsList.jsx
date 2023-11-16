6727d6ff3f5f94929daaad4f60111cd4b4b5b9e2
Aryan Jain <39268389+JainnyAryan@users.noreply.github.com>
2023-11-16 19:19:56 +0530

    text adjustment changes

--- src/components/employee/JobsList.jsx
+++ src/components/employee/JobsList.jsx
@@ -38,11 +38,11 @@ const JobsList = (props) => {
                 <div className={styles.jobTile}>
                   <img src={e.iconUrl} style={{ height: "10vh", objectFit: "contain", aspectRatio: "1/1", marginRight: "20px" }} />
                   <div style={{ flex: 3, display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
-                    <div style={{ display: "flex", height: "10vh", flexDirection: "column", alignItems: "start", justifyContent: "space-evenly", width: "50%" }}>
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "start", justifyContent: "space-evenly", width: "50%" }}>
                       <p style={{ margin: "0px", color: "grey" }}>{e.role}</p>
                       <p style={{ margin: "0px", color: "rgb(34,71,82)" }}>{e.orgName}</p>
                     </div>
-                    <div style={{ display: "flex", height: "10vh", flexDirection: "column", alignItems: "start", justifyContent: "space-evenly" }}>
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "start", justifyContent: "space-evenly" }}>
                       <span>
                         <MdLocationPin color="grey" />
                         <span style={{ color: "grey" }}>Location</span>
@@ -53,7 +53,7 @@ const JobsList = (props) => {
                 </div>
               </AccordionSummary>
               <AccordionDetails>
-                <JobDescSidePanel isMobileScreen={isMobileScreen} expandedJob={e}/>
+                <JobDescSidePanel isMobileScreen={isMobileScreen} expandedJob={e} />
               </AccordionDetails>
             </Accordion>
           )
