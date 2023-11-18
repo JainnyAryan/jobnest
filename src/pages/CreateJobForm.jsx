1f15ff2629abbafd23dc0a1857adebc5ef7abfd1
vaibhav <keshanvaibhav@gmail.com>
2023-11-18 17:36:41 +0530

    form validation

--- src/pages/CreateJobForm.jsx
+++ src/pages/CreateJobForm.jsx
@@ -1,17 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import styles from "./styles/CreateJobForm.module.css";
-import { useLocation, useNavigate } from "react-router-dom";
 import { ArrowForward } from "@mui/icons-material";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useFirebase } from "../context/firebaseContext";
 import axios from "axios";
 import { useUser } from "../context/userContext";
 import useAuth from "../context/useAuth";
 
-
 const CreateJobForm = () => {
   const userProvider = useUser();
   const user = useAuth();
   const navigate = useNavigate();
+
   if (!user.isEmployer) {
     navigate("/");
   }
@@ -20,42 +22,61 @@ const CreateJobForm = () => {
 
   const { image, progress, imageUrl, firebaseImageUrl, handleChange, handleUpload } = useFirebase();
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    formData.delete('jobIcon');
-    formData.append('iconUrl', firebaseImageUrl ? firebaseImageUrl : DEFAULT_IMG);
-    formData.append('employerId', userProvider.user._id);
-    formData.append('employer', JSON.stringify(userProvider.user));
-    // for (let [key, value] of formData.entries()) {
-    //   console.log(key, " : val : ", value);
-    // }
+  const validationSchema = Yup.object({
+    orgName: Yup.string().required("Organization Name is required"),
+    role: Yup.string().required("Role is required"),
+    locationType: Yup.string().required("Location Type is required"),
+    location: Yup.string().required("Job Location is required"),
+    salary: Yup.string().required("Salary is required"),
+    roleDescription: Yup.string().required("Job Description is required"),
+  });
 
-    axios.post("https://jobnest-backend.vercel.app/post_job", formData)
-      .then((res) => {
+  const formik = useFormik({
+    initialValues: {
+      jobIcon: null,
+      orgName: "",
+      role: "",
+      locationType: "",
+      location: "",
+      salary: "",
+      roleDescription: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: async (values) => {
+      const formData = new FormData();
+      formData.append("jobIcon", values.jobIcon);
+      formData.append("iconUrl", firebaseImageUrl ? firebaseImageUrl : DEFAULT_IMG);
+      formData.append("employerId", userProvider.user._id);
+      formData.append("employer", JSON.stringify(userProvider.user));
+      formData.append("orgName", values.orgName);
+      formData.append("role", values.role);
+      formData.append("locationType", values.locationType);
+      formData.append("location", values.location);
+      formData.append("salary", values.salary);
+      formData.append("roleDescription", values.roleDescription);
+
+      try {
+        const res = await axios.post("https://jobnest-backend.vercel.app/post_job", formData);
         console.log(res);
         navigate("/employer");
         alert("Job created!");
-      })
-      .catch((err) => {
-        console.log(err);
+      } catch (err) {
+        console.error(err);
         alert(err);
-      });
-
-  }
-
-
+      }
+    },
+  });
 
   return (
     <div className={styles.background_image}>
-      <form className={styles.formBox} onSubmit={handleSubmit}>
+      <form className={styles.formBox} onSubmit={formik.handleSubmit}>
         <h1>Create a Job</h1>
 
         <br />
 
         <div style={{ display: "flex", flexDirection: "column", marginBottom: "10px", maxHeight: "50vh" }}>
           <label htmlFor="jobIcon" className={styles.headingColor}>Job/Organiztion Icon:</label>
-          <input type="file" name="jobIcon" onChange={handleChange} />
+          <input type="file" name="jobIcon" onChange={(event) => { handleChange(event); formik.setFieldValue("jobIcon", event.currentTarget.files[0]); }} />
           <span>
             {imageUrl && <button type="button" onClick={handleUpload} style={{ width: "min-content", margin: "5px", padding: "5px 10px", borderRadius: "10px", backgroundColor: "rgb(108,228,242)", color: "rgb(35,36,35)", border: "none" }}>{firebaseImageUrl ? "Uploaded" : "Upload"}</button>}
             {progress !== 0 && <progress value={progress} max="100" />}
@@ -66,27 +87,81 @@ const CreateJobForm = () => {
         <label htmlFor="orgName" className={styles.headingColor}>
           Organization Name:
         </label>
-        <input type="text" name="orgName" className={styles.inputDimensions} placeholder="Enter the name of organization." />
+        <input
+          type="text"
+          name="orgName"
+          className={styles.inputDimensions}
+          placeholder="Enter the name of organization."
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.orgName}
+        />
+        {formik.touched.orgName && formik.errors.orgName && <div className={styles.errorText}>{formik.errors.orgName}</div>}
 
         <label htmlFor="role" className={styles.headingColor}>Role</label>
-        <input type="text" name="role" id="role" className={styles.inputDimensions} placeholder="Enter the role for the employee." />
+        <input
+          type="text"
+          name="role"
+          id="role"
+          className={styles.inputDimensions}
+          placeholder="Enter the role for the employee."
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.role}
+        />
+        {formik.touched.role && formik.errors.role && <div className={styles.errorText}>{formik.errors.role}</div>}
 
         <label htmlFor="locationType" className={styles.headingColor}>Location Type</label>
-        <select name="locationType" className={styles.inputDimensions}>
-          <option value="" disabled selected hidden>Choose the location type</option>
+        <select
+          name="locationType"
+          className={styles.inputDimensions}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.locationType}
+        >
+          <option value="" disabled hidden>Choose the location type</option>
           <option value="On Site">On Site</option>
           <option value="Work From Home">Work From Home</option>
           <option value="Hybrid">Hybrid</option>
         </select>
+        {formik.touched.locationType && formik.errors.locationType && <div className={styles.errorText}>{formik.errors.locationType}</div>}
 
         <label htmlFor="location" className={styles.headingColor}>Job Location</label>
-        <input type="text" name="location" placeholder="Enter the job location" className={styles.inputDimensions} />
+        <input
+          type="text"
+          name="location"
+          placeholder="Enter the job location"
+          className={styles.inputDimensions}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.location}
+        />
+        {formik.touched.location && formik.errors.location && <div className={styles.errorText}>{formik.errors.location}</div>}
 
         <label htmlFor="salary" className={styles.headingColor}>Salary</label>
-        <input type="text" name="salary" placeholder="Enter the salary of the job." className={styles.inputDimensions} />
+        <input
+          type="text"
+          name="salary"
+          placeholder="Enter the salary of the job."
+          className={styles.inputDimensions}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.salary}
+        />
+        {formik.touched.salary && formik.errors.salary && <div className={styles.errorText}>{formik.errors.salary}</div>}
 
         <label htmlFor="roleDescription" className={styles.headingColor}>Job Description</label>
-        <textarea name="roleDescription" width="100%" rows="3" placeholder="Write about the job description." className={styles.inputDimensions}></textarea>
+        <textarea
+          name="roleDescription"
+          width="100%"
+          rows="3"
+          placeholder="Write about the job description."
+          className={styles.inputDimensions}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.roleDescription}
+        ></textarea>
+        {formik.touched.roleDescription && formik.errors.roleDescription && <div className={styles.errorText}>{formik.errors.roleDescription}</div>}
 
         <button
           type="submit"
@@ -103,10 +178,11 @@ const CreateJobForm = () => {
             justifyContent: "center"
           }}
         >
-          Post Job <ArrowForward style={{ marginLeft: "1vw" }} /></button>
+          Post Job <ArrowForward style={{ marginLeft: "1vw" }} />
+        </button>
       </form>
     </div>
   );
-}
+};
 
 export default CreateJobForm;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import loginAnim from "../assets/anims/loginAnim.json";
 import Lottie from "react-lottie";
 import { useNavigate } from "react-router-dom";
@@ -11,43 +13,46 @@ import secureLocalStorage from "react-secure-storage";
 const LoginPage = () => {
   const navigate = useNavigate();
   const userProvider = useUser();
-  const [userInput, setuserInput] = useState("");
-  const [password, setPassword] = useState("");
 
-  const handleuserInput = (event) => {
-    setuserInput(event.target.value);
-  };
+  const validationSchema = Yup.object().shape({
+    userInput: Yup.string().required("Username or email is required"),
+    password: Yup.string().required("Password is required"),
+  });
 
-  const handlePassword = (event) => {
-    setPassword(event.target.value);
-  }
+  const formik = useFormik({
+    initialValues: {
+      userInput: "",
+      password: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      handleSubmit(values);
+    },
+  });
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    axios.post('https://jobnest-backend.vercel.app/login', { userInput, password })
-      .then(
-        result => {
-          const loginData = result.data;
-          if (loginData.status === true) {
-            delete loginData.data.password;
-            secureLocalStorage.setItem('userData', JSON.stringify(loginData.data));
-            userProvider.setUserData(loginData.data);
-            navigate("/");
-          }
-          else {
-            alert(loginData.message);
-          }
+  const handleSubmit = (values) => {
+    axios
+      .post("https://jobnest-backend.vercel.app/login", values)
+      .then((result) => {
+        const loginData = result.data;
+        if (loginData.status === true) {
+          delete loginData.data.password;
+          secureLocalStorage.setItem("userData", JSON.stringify(loginData.data));
+          userProvider.setUserData(loginData.data);
+          navigate("/");
+        } else {
+          alert(loginData.message);
         }
-      ).catch(err => {
-        alert(err);
       })
-  }
+      .catch((err) => {
+        alert(err);
+      });
+  };
 
   return (
     <div
       style={{
-        backgroundImage:
-          'url("/background1.jpg")',
+        backgroundImage: 'url("/background1.jpg")',
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
         backgroundPosition: "center center",
@@ -60,11 +65,13 @@ const LoginPage = () => {
     >
       <div>
         <div className={styles.box}>
-          <ArrowBack className={styles.backIcon} onClick={() => navigate("/")} fontSize="large" />
+          <ArrowBack
+            className={styles.backIcon}
+            onClick={() => navigate("/")}
+            fontSize="large"
+          />
 
-          <div
-            className={styles.loginBox}
-          >
+          <div className={styles.loginBox}>
             <div className={styles.lottieDiv}>
               <Lottie
                 options={{
@@ -85,62 +92,89 @@ const LoginPage = () => {
                 }}
               />
             </div>
-            <div
-              className={styles.formBox}
-            >
+            <div className={styles.formBox}>
               <img
                 src="/LOGO_transparent.png"
                 alt="LOGO-transparent"
                 border="0"
                 width={"100%"}
-                style={{ borderTopRightRadius: "20px", padding: "10% 5%" }}
+                style={{
+                  borderTopRightRadius: "20px",
+                  padding: "10% 5%",
+                }}
               ></img>
 
-              <div style={{ display: "flex", flexDirection: "column", padding: "15% 10%" }}>
-                <div className="form-group">
-                  <label htmlFor="userInput" style={{ color: "#6CE4F3" }}>
-                    Email / Username
-                  </label>
-                  <input
-                    type="text"
-                    id="userInput"
-                    placeholder="Enter username or email"
-                    className="form-control"
-                    style={{
-                      borderRadius: "8px",
-                      border: "1px solid #ccdce1",
-                    }}
-                    onChange={handleuserInput}
-                  />
-                </div>
+              <div
+                style={{ display: "flex", flexDirection: "column", padding: "15% 10%" }}
+              >
+                <form onSubmit={formik.handleSubmit}>
+                  <div className="form-group">
+                    <label htmlFor="userInput" style={{ color: "#6CE4F3" }}>
+                      Email / Username
+                    </label>
+                    <input
+                      type="text"
+                      id="userInput"
+                      placeholder="Enter username or email"
+                      className="form-control"
+                      style={{
+                        borderRadius: "8px",
+                        border: "1px solid #ccdce1",
+                      }}
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                      value={formik.values.userInput}
+                    />
+                    {formik.touched.userInput && formik.errors.userInput && (
+                      <div style={{ color: "red" }}>{formik.errors.userInput}</div>
+                    )}
+                  </div>
 
-                <div className="form-group mt-3">
-                  <label htmlFor="password" style={{ color: "#6CE4F3" }}>
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    id="password"
-                    placeholder="Enter the Password"
-                    className="form-control"
+                  <div className="form-group mt-3">
+                    <label htmlFor="password" style={{ color: "#6CE4F3" }}>
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      id="password"
+                      placeholder="Enter the Password"
+                      className="form-control"
+                      style={{
+                        borderRadius: "8px",
+                        border: "1px solid #ccdce1",
+                        textAlign: "left",
+                      }}
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                      value={formik.values.password}
+                    />
+                    {formik.touched.password && formik.errors.password && (
+                      <div style={{ color: "red" }}>{formik.errors.password}</div>
+                    )}
+                  </div>
+
+                  <button
+                    className="btn mt-4"
                     style={{
-                      borderRadius: "8px",
-                      border: "1px solid #ccdce1",
-                      textAlign: "left",
+                      borderRadius: "10px",
+                      backgroundColor: "#6CE4F3",
+                      color: "#232423",
                     }}
-                    onChange={handlePassword}
-                  />
-                </div>
-                <button
-                  className="btn mt-4"
-                  style={{ borderRadius: "10px", backgroundColor: "#6CE4F3", color: "#232423" }}
-                  onClick={handleSubmit}
-                >
-                  Login
-                </button>
+                    type="submit"
+                  >
+                    Login
+                  </button>
+                </form>
+
                 <p style={{ color: "#6CE4F3", marginTop: "2vh" }}>
                   New to JobNest?
-                  <a href="" style={{ marginLeft: "5px", color: "#6CE4F3" }} onClick={() => navigate('/register')}>Register Now</a>
+                  <a
+                    href=""
+                    style={{ marginLeft: "5px", color: "#6CE4F3" }}
+                    onClick={() => navigate("/register")}
+                  >
+                    Register Now
+                  </a>
                 </p>
               </div>
             </div>
